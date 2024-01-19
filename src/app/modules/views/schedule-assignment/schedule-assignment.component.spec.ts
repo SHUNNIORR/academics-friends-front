ff7bd1ebd150e276f7ac113250c433b30f85331a
joperez <joperez@bancolombia.com.co>
2024-01-19 12:00:35 -0500

    test,ajustes en el login y nuevo logo ig sistemas

--- src/app/modules/views/schedule-assignment/schedule-assignment.component.spec.ts
+++ src/app/modules/views/schedule-assignment/schedule-assignment.component.spec.ts
@@ -1,21 +1,221 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { ScheduleAssignmentComponent } from './schedule-assignment.component';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { MatSnackBarModule } from '@angular/material/snack-bar';
+import { MatTabsModule } from '@angular/material/tabs';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { DatePipe } from '@angular/common';
+import { MatDialog, MatDialogModule } from '@angular/material/dialog';
+import { AppModule } from 'src/app/app.module';
+import { ScheduleService } from '../../services/schedule/schedule.service';
+import { DialogService } from 'src/app/shared/services/dialog/dialog.service';
+import { CoreService } from 'src/app/core/services/core/core.service';
+import { of, throwError } from 'rxjs';
 
 describe('ScheduleAssignmentComponent', () => {
   let component: ScheduleAssignmentComponent;
   let fixture: ComponentFixture<ScheduleAssignmentComponent>;
-
+  let coreService: CoreService;
+  let scheduleService: ScheduleService;
+  let dialogService: DialogService
   beforeEach(() => {
     TestBed.configureTestingModule({
+      imports:[
+        HttpClientTestingModule,
+        MatSnackBarModule,
+        MatTabsModule,
+        BrowserAnimationsModule,
+        MatDialogModule,
+        AppModule
+      ],
+      providers:[
+        DatePipe
+      ],
       declarations: [ScheduleAssignmentComponent]
     });
     fixture = TestBed.createComponent(ScheduleAssignmentComponent);
     component = fixture.componentInstance;
+    coreService = TestBed.inject( CoreService);
+    scheduleService = TestBed.inject( ScheduleService);
+    dialogService = TestBed.inject( DialogService);
     fixture.detectChanges();
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+  it('getScheduleService', () => {
+    spyOn(scheduleService, 'getAllSchedule').and.returnValue(
+      of([{},{}])
+    );
+  
+    component.getScheduleService();
+  });
+  it('getScheduleService', () => {
+  
+    spyOn(scheduleService, 'getAllSchedule').and.returnValue(
+      of([])
+    );
+  
+    component.getScheduleService();
+  });
+  it('getScheduleService', () => {
+  
+    spyOn(scheduleService, 'getAllSchedule').and.returnValue(
+      throwError({error:{message:'error'}})
+    );
+  
+    component.getScheduleService();
+  });
+  it('saveScheduleService', () => {
+    spyOn(scheduleService, 'saveSchedule').and.returnValue(
+      of([{},{}])
+    );
+  
+    component.saveScheduleService({});
+  });
+  it('saveScheduleService', () => {
+  
+    spyOn(scheduleService, 'saveSchedule').and.returnValue(
+      of([])
+    );
+  
+    component.saveScheduleService({});
+  });
+  it('saveScheduleService', () => {
+  
+    spyOn(scheduleService, 'saveSchedule').and.returnValue(
+      throwError({error:{message:'error'}})
+    );
+  
+    component.saveScheduleService({});
+  });
+  it('getScheduleByEmailService', () => {
+    spyOn(scheduleService, 'getSchedulesByEmail').and.returnValue(
+      of([{},{}])
+    );
+  
+    component.getScheduleByEmailService('');
+  });
+  it('getScheduleByEmailService', () => {
+  
+    spyOn(scheduleService, 'getSchedulesByEmail').and.returnValue(
+      of([])
+    );
+  
+    component.getScheduleByEmailService('');
+  });
+  it('getScheduleByEmailService', () => {
+  
+    spyOn(scheduleService, 'getSchedulesByEmail').and.returnValue(
+      throwError({error:{message:'error'}})
+    );
+  
+    component.cancelScheduleService('');
+  });
+  it('cancelScheduleService', () => {
+    spyOn(scheduleService, 'cancelSchedule').and.returnValue(
+      of([{},{}])
+    );
+  
+    component.cancelScheduleService('');
+  });
+  it('cancelScheduleService', () => {
+  
+    spyOn(scheduleService, 'cancelSchedule').and.returnValue(
+      of([])
+    );
+  
+    component.cancelScheduleService('');
+  });
+  it('cancelScheduleService', () => {
+  
+    spyOn(scheduleService, 'cancelSchedule').and.returnValue(
+      throwError({error:{message:'error'}})
+    );
+  
+    component.cancelScheduleService('');
+  });
+  it('assignmentScheduleService', () => {
+    spyOn(scheduleService, 'assignSchedule').and.returnValue(
+      of([{},{}])
+    );
+  
+    component.assignmentScheduleService('');
+  });
+  it('assignmentScheduleService', () => {
+  
+    spyOn(scheduleService, 'assignSchedule').and.returnValue(
+      of([])
+    );
+  
+    component.assignmentScheduleService('');
+  });
+  it('assignmentScheduleService', () => {
+  
+    spyOn(scheduleService, 'assignSchedule').and.returnValue(
+      throwError({error:{message:'error'}})
+    );
+  
+    component.assignmentScheduleService('');
+  });
+  it('should generate next hours', () => {
+    const initialHour = '08:00';
+    const result = component.generarHorasSiguientes(initialHour);
+
+    // Assuming incrementing by 1 hour until 18:00
+    const expectedHours = [
+      { value: '09:00', label: '09:00' },
+      { value: '10:00', label: '10:00' },
+      { value: '11:00', label: '11:00' },
+      { value: '12:00', label: '12:00' },
+    ];
+
+    expect(result).toEqual(expectedHours);
+  });
+
+  it('should increment hour correctly', () => {
+    const initialHour = '08:00';
+    const result = component.incrementarHora(initialHour);
+
+    expect(result).toEqual('09:00');
+  });
+
+  it('should format date to string', () => {
+    const date = new Date('2000-01-01T08:00');
+    const result = component.formatearHora(date);
+
+    expect(result).toEqual('08:00');
+  });
+  it('should add one hour to the start time', () => {
+    const startTime = '08:30';
+    const result = component.addOneHour(startTime);
+
+    expect(result).toEqual({ endHour: '09:30' });
+  });
+
+  it('should get day in Spanish', () => {
+    const days = ['MONDAY', 'TUESDAY', 'WEDNESDAY', 'THURSDAY', 'FRIDAY'];
+    
+    days.forEach(day => {
+      const result = component.getDayInSpanish(day);
+      const expected = {
+        'MONDAY': 'Lunes',
+        'TUESDAY': 'Martes',
+        'WEDNESDAY': 'Miércoles',
+        'THURSDAY': 'Jueves',
+        'FRIDAY': 'Viernes',
+      }[day];
+
+      expect(result).toEqual(expected!);
+    });
+
+    // Test with an invalid day
+    const invalidDay = 'SUNDAY';
+    const result = component.getDayInSpanish(invalidDay);
+    const expected = 'Día no válido';
+
+    expect(result).toEqual(expected);
   });
 });
--- src/app/shared/components/dinamic-dialog/dinamic-dialog.component.spec.ts
+++ src/app/shared/components/dinamic-dialog/dinamic-dialog.component.spec.ts
@@ -1,21 +1,127 @@
+import { DynamicFormData } from 'src/app/shared/models/DynamicFormData';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { DinamicDialogComponent } from './dinamic-dialog.component';
+import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { MatDatepickerModule } from '@angular/material/datepicker';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { NgxMatFileInputModule } from '@angular-material-components/file-input';
+import { MatSelectModule } from '@angular/material/select';
+import { MatNativeDateModule } from '@angular/material/core';
+import { NgxMatDatetimePickerModule, NgxMatNativeDateModule, NgxMatTimepickerModule } from '@angular-material-components/datetime-picker';
+import { MatPaginatorModule } from '@angular/material/paginator';
+import { MAT_DIALOG_DATA, MatDialog, MatDialogModule, MatDialogRef } from '@angular/material/dialog';
+import { DialogService } from '../../services/dialog/dialog.service';
+import { SEARCH_ACADEMIC_FRIEND } from 'src/app/modules/metadata/academic-friend/academic-friend.metadata';
+import { MatInputModule } from '@angular/material/input';
 
 describe('DinamicDialogComponent', () => {
   let component: DinamicDialogComponent;
   let fixture: ComponentFixture<DinamicDialogComponent>;
-
+  const dialogMock = {
+    close: () => { }
+    };
   beforeEach(() => {
     TestBed.configureTestingModule({
+      imports: [
+        ReactiveFormsModule,
+        MatDatepickerModule,
+        BrowserAnimationsModule,
+        MatFormFieldModule,
+        NgxMatFileInputModule,
+        MatSelectModule,
+        MatNativeDateModule,
+        NgxMatDatetimePickerModule,
+        NgxMatTimepickerModule,
+        NgxMatNativeDateModule,
+        MatPaginatorModule,
+        MatDialogModule,
+        MatInputModule
+      ],
+      providers: [
+        {provide:MatDialogRef , useValue:dialogMock },
+
+        { provide: MAT_DIALOG_DATA, useValue: {} }
+      ],
       declarations: [DinamicDialogComponent]
     });
     fixture = TestBed.createComponent(DinamicDialogComponent);
     component = fixture.componentInstance;
+    component.formData = SEARCH_ACADEMIC_FRIEND
+    component.formData.fields.push({
+      key: 'dateRange',
+      label: 'Fechas de inscripción',
+      type: 'date-range',
+      required: true,
+      dateRange: new FormGroup({
+        start: new FormControl<Date | null>(null,Validators.required),
+        end: new FormControl<Date | null>(null,Validators.required),
+      }),
+      startDateName: new FormControl<Date | null>(null,Validators.required),
+      endDateName: new FormControl<Date | null>(null,Validators.required),
+    },)
     fixture.detectChanges();
+    
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+  it('should initialize form', () => {
+
+    component.ngOnInit();
+    expect(component.form).toBeTruthy();
+  });
+  it('should toggle password visibility', () => {
+    expect(component.hide).toBeTruthy();
+
+    component.togglePasswordVisibility(new Event('click'));
+
+    expect(component.hide).toBeFalsy();
+  });
+  it('should submit form', () => {
+    
+    const formValue = { academicFriendCode: '1', dateRange: Object({ start: null, end: null })};
+    spyOn(component.formSubmit, 'emit');
+    spyOn(component.dialogRef, 'close');
+    spyOn(component.dateRange,'reset')
+    spyOn(component.form,'reset')
+    component.itsDateRange=true;
+    component.form.addControl('dateRange', new FormControl('', Validators.required))
+    component.form.setValue(formValue);
+    component.onSubmit();
+
+    expect(component.formSubmit.emit).toHaveBeenCalledWith(formValue);
+    expect(component.dialogRef.close).toHaveBeenCalledWith(formValue);
+    expect(component.dateRange.reset).toHaveBeenCalled();
+    expect(component.form.reset).toHaveBeenCalled();
+  });
+  
+  it('should cancel form', () => {
+    spyOn(component.dialogRef, 'close');
+    component.onCancel();
+    expect(component.dialogRef.close).toHaveBeenCalled();
+  });
+  it('should get form controls using f()', () => {
+    component.ngOnInit();
+  
+    const formControls = component.f;
+  
+    expect(formControls).toBeTruthy();
+  });
+  
+  it('should get current date in DD/MM/YYYY format', () => {
+    const currentDate = new Date();
+    const expectedDate = currentDate
+      .toJSON()
+      .slice(0, 10)
+      .split('-')
+      .reverse()
+      .join('/');
+  
+    const currentDateFormatted = component.getCurrentDate();
+  
+    expect(currentDateFormatted).toEqual(expectedDate);
   });
 });
